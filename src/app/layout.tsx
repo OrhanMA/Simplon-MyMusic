2b7b81a7c8db6e73501a1c3e87d1b9079a2d2fb7
OrhanMA <orhan.madi.assani@gmail.com>
2023-06-30 15:30:53 +0200

    layout back in tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import Header from "@/components/Header";
 import "./globals.css";
 
-export default function RootLayout() {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
